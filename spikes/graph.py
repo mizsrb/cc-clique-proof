3d55d6a9e21409f144ad03af0b09b6164228f5ed
S. Revilla <alu0100947441@ull.edu.es>
2019-01-17 22:09:32 +0000

    Pipfile with matplotlib and graph spikes

---
+++ spikes/graph.py
@@ -0,0 +1,19 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+nodes = [1, 2, 3]
+edges = [(1, 2), (2, 3)]
+
+G = nx.Graph()
+#G.add_nodes_from(nodes)
+G.add_edges_from(edges)
+
+edges = G.edges()
+for edge in edges:
+    string = ""
+    for x in edge:
+        string += str(x) + ' '
+    print(string)
+
+nx.draw_networkx(G)
+plt.show()
