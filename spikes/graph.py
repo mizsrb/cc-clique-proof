ab4122e107cde5144ed483fe27222d2246cc42d5
S. Revilla <alu0100947441@ull.edu.es>
2019-01-18 00:48:11 +0000

    Spikes file

--- spikes/graph.py
+++ spikes/graph.py
@@ -2,7 +2,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 nodes = [1, 2, 3]
-edges = [(1, 2), (2, 3)]
+edges = [(1, 2), (2, 3), (1, 3)]
 
 G = nx.Graph()
 #G.add_nodes_from(nodes)
@@ -15,5 +15,14 @@ for edge in edges:
         string += str(x) + ' '
     print(string)
 
-nx.draw_networkx(G)
+cliques = list(nx.find_cliques(G))
+
+for clique in cliques:
+    print(clique)
+    input()
+    for node in clique:
+
+
+nx.draw(G)
 plt.show()
+input()
